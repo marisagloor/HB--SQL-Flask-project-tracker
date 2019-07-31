4f98a97037a8228124f308071c75792f5358520d
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-31 12:56:16 -0700

    incorporated html

--- hackbright_web.py
+++ hackbright_web.py
@@ -7,15 +7,25 @@ import hackbright
 app = Flask(__name__)
 
 
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+    return render_template("student_search.html")
+
 @app.route("/student")
 def get_student():
     """Show information about a student."""
 
-    github = "jhacks"
+    github = request.args.get('github')
 
     first, last, github = hackbright.get_student_by_github(github)
 
-    return "{} is the GitHub account for {} {}".format(github, first, last)
+    # return f"{github} is the GitHub account for {first} {last}"
+
+    return render_template("student_info.html", 
+                            first=first,
+                            last=last, 
+                            github=github)
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
